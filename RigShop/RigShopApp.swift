9851475977fe7c9ebb24d0f7b0703a3171d76744
Jerry Chen <310314454+jerrylion0726@users.noreply.github.com>
2026-08-02 16:10:35 -0500

    Initial commit: RigShop project setup

---
+++ RigShop/RigShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  RigShopApp.swift
+//  RigShop
+//
+//  Created by 陈禹铭 on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RigShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
